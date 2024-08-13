46951812455b6bfda487cb738347c95d90860431
SakshiBawankar28 <sakshibawabkar@gmail.com>
2024-08-13 21:53:15 +0530

    Frontend

--- Frontend/sparkle-with-us/src/components/Header.js
+++ Frontend/sparkle-with-us/src/components/Header.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import './Style.css';
 import { Link } from 'react-router-dom';
+// import './Style.css';
+import './css/Style.css';
 
 
 const Header = () => {
   return (
     <header className="header">
       <img src={require('../images/images.png')} alt="Salon Logo" className="logo" />
-      <nav>
+      <nav className="navbar">
         <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/services">Services</Link></li>
-        <li><Link to="/stylists">Stylists</Link></li>
-        <li><Link to="/booking">Book Appointment</Link></li>
-        <li><Link to="/about-us">About Us</Link></li>
-        <li><Link to="/contact-us">Contact Us</Link></li>
-        <li><Link to="/reviews">Reviews</Link></li>
-        <li><Link to="/login">Login</Link></li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/aboutus">About Us</Link></li>
+          <li><Link to="/services">Services</Link></li>
+          <li><Link to="/contactus">Contact Us</Link></li>
+          {/* <li><Link to="/admin">Admin</Link></li> */}
+          {/* <li><Link to="/signup">Sign Up</Link></li> */}
+          <li><Link to="/login">Login</Link></li>
         </ul>
       </nav>
     </header>
   );
-};
+}
 
 export default Header;
